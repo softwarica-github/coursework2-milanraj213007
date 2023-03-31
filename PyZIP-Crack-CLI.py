e5657bfcbe1f010289e144576790510d9eb89a21
Korozin <90534409+Korozin@users.noreply.github.com>
2023-03-30 22:06:16 -0400

    logic changes
    - character set is now an optional argument. If not provided it has a default setting.
    - if wordlist is exhausted, will switch to bruteforce under the same min, max and charset provided
    - now shows which password it's currently on with the wordlist (x out of x)
    - now shows which iteration and cycle brute force is on (x attempts on cycle x). Can't show all possible combos due to performance reasons.
    - Now shows how many attempts the crack took on both fail and success.

--- PyZIP-Crack-CLI.py
+++ PyZIP-Crack-CLI.py
@@ -1,4 +1,10 @@
-import itertools, argparse, zipfile, chardet, signal, sys, threading
+import itertools
+import argparse
+import zipfile
+import chardet
+import signal
+import sys
+import threading
 
 # Define a global variable to signal the password cracking loop to stop
 stop_event = threading.Event()
@@ -10,53 +16,87 @@ def signal_handler(sig, frame):
     stop_event.set()
     sys.exit()
 
-def extract_zip(zip_file, start_length, max_length, wordlist):
-    # Loop through all possible password combinations
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};':<>,./?|"
+def extract_zip(zip_file, start_length, max_length, charset, wordlist=None):
+    # filler to prevent print artifacting
+    filler = " " * (max_length // 2) # use // to prevent float values
     
+    # Loop through all possible password combinations
     if wordlist:
-        # Detect the encoding of the wordlist file
         with open(wordlist, 'r', errors='ignore') as f:
             passwords = f.read().splitlines()
-            
+        
         # Filter the passwords list to only include passwords within the desired length range
         passwords = [password for password in passwords if start_length <= len(password) <= max_length]
-    else:
-        # Generate all possible password combinations
-        passwords = (''.join(password) for length in range(start_length, max_length+1) for password in itertools.product(charset, repeat=length))
 
-    for password in passwords:
-        ''' Set comments according to which print method you'd like '''
+        # Print a message indicating that the dict attack is starting
+        print("Starting dictionary attack...")
+
+        # Use the wordlist until it is exhausted, then switch to the brute-force attack
+        for i, password in enumerate(passwords, start=1):
+            if stop_event.is_set():
+                return None
+
+            progress = f"Trying password from wordlist: {password} (on: {i} out of: {len(passwords)}){filler}"
+            print("\r" + progress, end="", flush=True)
+
+            try:
+                zip_file.extractall(pwd=password.encode())
+                print('\n\nPassword found:', password)
+                print('Total attempts:', i)   # Print the total number of attempts made
+                return password
+            except Exception:
+                pass
+        
+        print('\nWordlist exhausted, switching to brute-force attack...')
+
+    # Generate all possible password combinations
+    passwords = (''.join(password) for length in range(start_length, max_length+1) for password in     itertools.product(charset, repeat=length))
+
+    # Print a message indicating that the brute-force attack is starting
+    print("\nStarting brute-force attack...")
+
+    for i, password in enumerate(passwords, start=1):
+        if len(password) > max_length:
+            break
+        
         if stop_event.is_set():
             return None
-            
-        try:
-            password = password.decode('utf-8')
-        except:
-            password = password
         
-        print("\rTrying password: {:<{}}".format(password[:max_length], max_length), end="", flush=True) # Overwrite line every print
-        #print("Trying password:", password) # New line every print
+        progress = f"Trying password from wordlist: {password} (on iteration: {i} of cycle: {len(password)})"
+        print("\r" + progress, end="", flush=True)
 
         # Attempt to extract the ZIP file with the current password
         try:
             zip_file.extractall(pwd=password.encode())
+            print('\n\nPassword found:', password)
+            print('Total attempts:', i)   # Print the total number of attempts made
             return password
-        except:
+        except Exception:
+            global count
+            count = i
             pass
 
-    # If no password was found, return None
+    # If no password was found, print the total number of attempts made and return None
+    print('\n\nPassword not found.')
+    print('Total attempts:', count)
     return None
+
 
 if __name__ == '__main__':
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Extract a ZIP file using a brute-force attack.')
-    parser.add_argument('-zip', type=str, help='The path to the ZIP file.')
+    parser.add_argument('-zip', type=str, required=True, help='The path to the ZIP file.')
     parser.add_argument('-min', type=int, default=1, help='The starting length of the password to try.')
     parser.add_argument('-max', type=int, default=8, help='The maximum length of the password to try.')
     parser.add_argument('-wordlist', type=str, help='The path to a file containing a list of possible passwords.')
-    args = parser.parse_args()   
-    
+    parser.add_argument('-charset', type=str, default='abcdefghijklmnopqrstuvwxyz', help='The character set to use for the brute-force attack.')
+    args = parser.parse_args()
+
+    # Check if no arguments were provided
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
     # Register the signal handler
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -64,13 +104,7 @@ if __name__ == '__main__':
     try:
         zip_file = zipfile.ZipFile(args.zip)
     except Exception as e:
-        parser.print_help()
-        sys.exit()
+        parser.error('Failed to open ZIP file: ' + str(e))
 
     # Extract the ZIP file using a brute-force attack
-    password = extract_zip(zip_file, args.min, args.max, args.wordlist)
-
-    if password is None:
-        print('\nPassword not found.')
-    else:
-        print('\nPassword found:', password)
+    password = extract_zip(zip_file, args.min, args.max, args.charset, args.wordlist)
